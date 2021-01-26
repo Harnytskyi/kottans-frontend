1a75d3f4150fe14ee894d7a6922ad070bd14ccc9
Harnytskyi <andrij.garnytskiy@gmail.com>
2021-01-26 13:57:41 +0200

    added search and sorting

--- task_friends_app/js/script.js
+++ task_friends_app/js/script.js
@@ -1,6 +1,11 @@
 const GRID = document.getElementById("allFriends");
 const CHECKBOX_MAN = document.getElementById("gender-male");
 const CHECKBOX_WOMAN = document.getElementById("gender-female");
+const SORT_AZ = document.getElementById("a-z");
+const SORT_ZA = document.getElementById("z-a");
+const SEARCH_INPUT = document.getElementById('search');
+const SORT_09 = document.getElementById("0-9");
+const SORT_90 = document.getElementById("9-0");
 const FRIENDS = [
     {
         name: "Tom",
@@ -18,18 +23,22 @@ const FRIENDS = [
     }
 ]
 
-function displayFriends(){
-    const ALL_FRIENDS = document.createDocumentFragment();
-    let selectedFriends;
+let selectedFriends = [...FRIENDS];
+function filter(){
     if(male == true && female == true)
     selectedFriends = FRIENDS;
     else if(male == true && female == false)
     selectedFriends = FRIENDS.filter(item => item.gender == "male")
     else if(male == false && female == true)
     selectedFriends = FRIENDS.filter(item => item.gender == "female")
+}
+function displayFriends(){
+    const ALL_FRIENDS = document.createDocumentFragment();
+    
     GRID.innerHTML ='';
     selectedFriends.forEach(item => {
         const card = document.createElement('div');
+        card.id = item.name;
         card.classList.add('card');
         const name = document.createElement('p');
         name.classList.add("name");
@@ -46,20 +55,18 @@ function displayFriends(){
         card.append(phone);
         const email = document.createElement('button');
         const emaillink = document.createElement('a');
+        email.append("SEND MESSAGE");
+        emaillink.append(email);
         emaillink.href = "mailto:" + item.email;
-        emaillink.append("SEND MESSAGE");
-        email.append(emaillink);
-        card.append(email);
+        card.append(emaillink);
         ALL_FRIENDS.append(card);
     });
     GRID.append(ALL_FRIENDS);
+    search();
 }
 let male = true;
 let female = true;
-function filter(mass, sex){
-    let newMass = mass.filter(item => item.gender == sex)
-    console.log(newMass);
-}
+
 
 // function change(sex){
 //     filter(FRIENDS, sex);
@@ -102,8 +109,23 @@ function changeFilterStatus(checkbox, sex){
         }
         console.log(sex + "false");
     }
+    filter();
     displayFriends();
 }
+
+function search(){
+    let searchRequest = SEARCH_INPUT.value.toUpperCase();
+    selectedFriends.forEach(item =>{
+        const FRIEND = document.getElementById(item.name)
+        FRIEND.classList.remove('hidden');
+        if(item.name.toUpperCase().indexOf(searchRequest) == -1){
+            FRIEND.classList.add('hidden');
+        }
+    })
+    //displayFriends();
+}
+
+
 
 CHECKBOX_MAN.addEventListener('change', function() {
     changeFilterStatus(CHECKBOX_MAN, "male");
@@ -113,5 +135,34 @@ CHECKBOX_WOMAN.addEventListener('change', function() {
     changeFilterStatus(CHECKBOX_WOMAN, "female");
     //launchFilter(CHECKBOX_WOMAN, "female");
 });
+SORT_AZ.addEventListener('click', function(){
+    sortFriends("name");
+})
+SORT_ZA.addEventListener('click', function(){
+    sortFriends("name", "descending");
+})
+SORT_09.addEventListener('click', function(){
+    sortFriends("age" );
+})
+
+SORT_90.addEventListener('click', function(){
+    sortFriends("age", "descending");
+})
+
+function sortFriends(criterion, order){
+    FRIENDS.sort((a,b)=>{
+        //let ec = eval(criterion);
+        if(order == "descending"){
+            let c = a;
+            a = b;
+            b=c;
+        }
+        if(a[criterion] > b[criterion]) return 1;
+        if(a[criterion] == b[criterion]) return 0;
+        if(a[criterion] < b[criterion]) return -1;
+    })
+    filter();
+    displayFriends();
+}
 //CHECKBOX_MAN.addEventListener('change', change("male"));
 displayFriends();
