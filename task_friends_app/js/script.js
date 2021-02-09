76db723cccad48f1bd643ebcd9f0d9612c7f164b
Harnytskyi <andrij.garnytskiy@gmail.com>
2021-02-09 17:11:43 +0200

    simplified sorting function, added radio buttons

--- task_friends_app/js/script.js
+++ task_friends_app/js/script.js
@@ -1,15 +1,20 @@
 const GRID = document.getElementById("allFriends");
 const CHECKBOX_MAN = document.getElementById("gender-male");
 const CHECKBOX_WOMAN = document.getElementById("gender-female");
-const SORT_AZ = document.getElementById("a-z");
-const SORT_ZA = document.getElementById("z-a");
+const SORT_ALPHABET_ASCEND = document.getElementById("a-z");
+const SORT_ALPHABET_DESCEND = document.getElementById("z-a");
 const SEARCH_INPUT = document.getElementById('search');
-const SORT_09 = document.getElementById("0-9");
-const SORT_90 = document.getElementById("9-0");
+const SORT_AGE_ASCEND = document.getElementById("0-9");
+const SORT_AGE_DESCEND = document.getElementById("9-0");
 let friends = [];
+let gender = {
+    male: true,
+    female: true
+}
+
 
 let selectedFriends = [...friends];
-function filter() {
+function filterByGender() {
     if (gender.male == true && gender.female == true)
         selectedFriends = friends;
     else if (gender.male == true && gender.female == false)
@@ -20,13 +25,13 @@ function filter() {
 function displayFriends() {
     GRID.innerHTML = '';
     selectedFriends.forEach(item => {
-        GRID.innerHTML +=  `<div id="${item.name}" class="card">
+        GRID.innerHTML += `<div id="${item.name}" class="card">
             <img class="photo" src="${item.photo}">
             <p class="name">${item.name}</p>
             <p class="age">Age: ${item.age}</p>
-            <a class="phone" href="${item.phone}">${item.phone}</a>
+            <a class="phone" href="tel:${item.phone}">${item.phone}</a>
             <div class="email">
-                <a href="${item.email}">
+                <a href="mailto:${item.email}">
                     <button class="button-message">SEND MESSAGE</button>
                 </a>
             </div>
@@ -34,30 +39,26 @@ function displayFriends() {
     });
     search();
 }
-let gender = {
-    male: true,
-    female: true
-}
 
-function changeFilterStatus(checkbox, sex) {
+function changeFilterStatus(checkbox) {
     if (checkbox.checked) {
-        gender[sex] = true;
+        gender[checkbox.value] = true;
     } else {
-        if (sex == "male") {
+        if (checkbox.value == "male") {
             if (gender.female == false) {
                 gender.female = true;
                 CHECKBOX_WOMAN.click();
             }
         }
-        else if (sex == "female") {
+        else if (checkbox.value == "female") {
             if (gender.male == false) {
                 gender.male = true;
                 CHECKBOX_MAN.click();
             }
         }
-        gender[sex] = false;
+        gender[checkbox.value] = false;
     }
-    filter();
+    filterByGender();
     displayFriends();
 }
 
@@ -72,7 +73,25 @@ function search() {
     })
 }
 
-function sortFriends(criterion, order) {
+function sortFriends(value) {
+    let criterion;
+    let order;
+    switch (value) {
+        case 'alphabet-ascend':
+            criterion = 'name';
+            break;
+        case 'alphabet-descend':
+            criterion = 'name';
+            order = 'descending';
+            break;
+        case 'age-ascend':
+            criterion = 'age';
+            break;
+        case 'age-descend':
+            criterion = 'age';
+            order = 'descending';
+            break;
+    }
     friends.sort((a, b) => {
         if (order == "descending") {
             let c = a;
@@ -83,54 +102,54 @@ function sortFriends(criterion, order) {
         if (a[criterion] == b[criterion]) return 0;
         if (a[criterion] < b[criterion]) return -1;
     })
-    filter();
+    filterByGender();
     displayFriends();
 }
 
 CHECKBOX_MAN.addEventListener('change', function () {
-    changeFilterStatus(CHECKBOX_MAN, "male");
+    changeFilterStatus(CHECKBOX_MAN);
 });
 CHECKBOX_WOMAN.addEventListener('change', function () {
-    changeFilterStatus(CHECKBOX_WOMAN, "female");
+    changeFilterStatus(CHECKBOX_WOMAN);
 });
-SORT_AZ.addEventListener('click', function () {
-    sortFriends("name");
+SORT_ALPHABET_ASCEND.addEventListener('change', function () {
+    sortFriends(this.value);
 })
-SORT_ZA.addEventListener('click', function () {
-    sortFriends("name", "descending");
+SORT_ALPHABET_DESCEND.addEventListener('change', function () {
+    sortFriends(this.value);
 })
-SORT_09.addEventListener('click', function () {
-    sortFriends("age");
-})
-
-SORT_90.addEventListener('click', function () {
-    sortFriends("age", "descending");
+SORT_AGE_ASCEND.addEventListener('change', function () {
+    sortFriends(this.value);
 })
 
-function fetchUsers(){
-    let targetUrl  = "https://randomuser.me/api/?results=30&nat=au,ca,ch,de,dk,es,fr,gb,ie,no,nl,nz,us"
+SORT_AGE_DESCEND.addEventListener('change', function () {
+    sortFriends(this.value);
+})
 
-    fetch(targetUrl, {metod: "get"})
-    .then((response) => {
-      let json = response.json();
-      if (response.status >= 200 && response.status < 300) {
-        return json;
-      }
-      else {
-        return error;
-      }
-    }).then((json) => {
-      saveUsers(json.results);
-    })
-    .catch(function(error) {
-      showErrorMessage(error);
-    });
+function fetchUsers() {
+    let targetUrl = "https://randomuser.me/api/?results=30&nat=au,ca,ch,de,dk,es,fr,gb,ie,no,nl,nz,us"
+
+    fetch(targetUrl, { metod: "get" })
+        .then((response) => {
+            let json = response.json();
+            if (response.status >= 200 && response.status < 300) {
+                return json;
+            }
+            else {
+                return error;
+            }
+        }).then((json) => {
+            saveUsers(json.results);
+        })
+        .catch(function (error) {
+            showErrorMessage(error);
+        });
 }
 
-function saveUsers(usersArray){
+function saveUsers(usersArray) {
     usersArray.forEach(user => {
-        let friend ={
-            name: user.name.first +" "+ user.name.last,
+        let friend = {
+            name: user.name.first + " " + user.name.last,
             age: user.dob.age,
             phone: user.cell,
             email: user.email,
@@ -142,7 +161,7 @@ function saveUsers(usersArray){
     selectedFriends = [...friends];
     displayFriends();
 }
-function showErrorMessage(error){
+function showErrorMessage(error) {
     const field = document.getElementById("contentField");
     field.classList.add('error-text');
     field.innerHTML = `something wrong: <br>${error} <br> please reload the page`;
